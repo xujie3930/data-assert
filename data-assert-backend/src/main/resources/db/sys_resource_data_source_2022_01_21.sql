1f42bb5d9410a62daa4bbe65a718c0f56a0b4346
xujie <xujie-bl@bestpay.com.cn>
2022-01-20 11:50:19 +0800

    修改脚本

--- data-assert-backend/src/main/resources/db/sys_resource_data_source_2022_01_21.sql
+++ data-assert-backend/src/main/resources/db/sys_resource_data_source_2022_01_21.sql
@@ -37,8 +37,8 @@ INSERT INTO `sys_resource` (`sys_resource_id`, `name`, `is_leaf`, `code`, `paren
                             `layout`, `order_by`, `remark`, `create_time`, `creator_id`, `creator_name`,
                             `creator_depart_id`, `creator_depart_name`, `update_time`, `updater_id`, `updater_name`,
                             `updater_depart_id`, `updater_depart_name`, `need_auth`)
-VALUES ('925068623448375896', '是否存在重复接口名', 1, 'L-169', '925068022387834880', 1,
-        '/*/table/setting/hasExistInterfaceName', 1, 3, NULL, NULL, '2021-12-29 12:43:14', NULL, NULL, NULL, NULL,
+VALUES ('925068623448385496', '是否存在开放状态的表', 1, 'L-180', '925068022387834880', 1,
+        '/*/resource/table/hasExistOpenExternalState', 1, 3, NULL, NULL, '2021-12-29 12:43:14', NULL, NULL, NULL, NULL,
         '2021-12-29 12:43:14', NULL, NULL, NULL, NULL, NULL);
 INSERT INTO `sys_resource` (`sys_resource_id`, `name`, `is_leaf`, `code`, `parent_id`, `status`, `url`, `type`,
                             `layout`, `order_by`, `remark`, `create_time`, `creator_id`, `creator_name`,
