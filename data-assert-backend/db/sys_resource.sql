456c7c4ba5e8558fb1e6673e11e8054f9f41fc83
xujie <xujie-bl@bestpay.com.cn>
2022-03-25 15:04:31 +0800

    增加权限sql

--- data-assert-backend/db/sys_resource.sql
+++ data-assert-backend/db/sys_resource.sql
@@ -223,6 +223,6 @@ INSERT INTO `sys_resource` (`sys_resource_id`, `name`, `is_leaf`, `code`, `paren
                             `layout`, `order_by`, `remark`, `create_time`, `creator_id`, `creator_name`,
                             `creator_depart_id`, `creator_depart_name`, `update_time`, `updater_id`, `updater_name`,
                             `updater_depart_id`, `updater_depart_name`, `need_auth`)
-VALUES ('925068623448375869', '获取主数据列表', 1, 'L-169', '925068022387834880', 1,
+VALUES ('925068623448375869', '获取主数据列表', 1, 'L-215', '925068022387834880', 1,
         '/*/masterData/getmasterData', 1, 3, NULL, NULL, '2022-02-25 12:43:14', NULL, NULL, NULL, NULL,
         '2021-12-29 12:43:14', NULL, NULL, NULL, NULL, NULL);
