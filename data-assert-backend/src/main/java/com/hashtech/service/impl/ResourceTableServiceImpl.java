e6861ae207ade968a23ccd7b2c8532235631ed63
xujie <xujie-bl@bestpay.com.cn>
2022-03-30 10:19:10 +0800

    --story=1024244 --user=徐捷 【资源目录】-进行接口设置后删除资源表操作-后端 https://www.tapd.cn/64558269/s/1460336

--- data-assert-backend/src/main/java/com/hashtech/service/impl/ResourceTableServiceImpl.java
+++ data-assert-backend/src/main/java/com/hashtech/service/impl/ResourceTableServiceImpl.java
@@ -27,6 +27,7 @@ import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
