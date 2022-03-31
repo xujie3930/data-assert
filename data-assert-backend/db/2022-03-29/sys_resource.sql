0dc2cde9de692bc8bad5706fc040c4bd318a035a
liyanhui <liyanhui@hashvictor.com>
2022-03-31 13:22:36 +0000

    Update sys_resource.sql

--- data-assert-backend/db/2022-03-29/sys_resource.sql
+++ data-assert-backend/db/2022-03-29/sys_resource.sql
@@ -7,5 +7,5 @@ INSERT INTO `sys_resource` (`sys_resource_id`, `name`, `is_leaf`, `code`, `paren
                             `creator_depart_id`, `creator_depart_name`, `update_time`, `updater_id`, `updater_name`,
                             `updater_depart_id`, `updater_depart_name`, `need_auth`)
 VALUES ('925068623448375869', '获取主数据列表', 1, 'L-217', '925068022387834880', 1,
-        '/*/masterData/getmasterData', 1, 3, NULL, NULL, '2022-02-25 12:43:14', NULL, NULL, NULL, NULL,
+        '/**/masterData/getmasterData', 1, 3, NULL, NULL, '2022-02-25 12:43:14', NULL, NULL, NULL, NULL,
         '2021-12-29 12:43:14', NULL, NULL, NULL, NULL, NULL);
