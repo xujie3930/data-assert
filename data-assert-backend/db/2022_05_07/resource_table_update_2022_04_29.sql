aa7d1c40cfb49ea90f318214c6ae072c4982d6af
xujie <xujie-bl@bestpay.com.cn>
2022-05-09 13:44:29 +0800

    增加批量修复主数据的sql

--- data-assert-backend/db/2022_05_07/resource_table_update_2022_04_29.sql
+++
@@ -1,6 +0,0 @@
-use daas;
-ALTER TABLE master_data ADD `theme_id` varchar(32) DEFAULT NULL COMMENT '对应主题数据';
-update theme_resource set name = '法人主数据'  where name = '法人主题' and del_flag = 'N';
-update theme_resource set name = '自然人主数据'  where name = '自然人主题'and del_flag = 'N';
-update master_data set theme_id = (SELECT id from theme_resource where name = '法人主数据' and del_flag = 'N') where id = 1;
-update master_data set theme_id = (SELECT id from theme_resource where name = '自然人主数据' and del_flag = 'N') where id = 2;
