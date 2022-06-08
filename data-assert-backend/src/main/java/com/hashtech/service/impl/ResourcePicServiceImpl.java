4acc4f6a39c69962ea3398ada6d591d1825cd597
xujie <xujie-bl@bestpay.com.cn>
2022-06-08 10:10:16 +0800

    提交sql

--- data-assert-backend/src/main/java/com/hashtech/service/impl/ResourcePicServiceImpl.java
+++ data-assert-backend/src/main/java/com/hashtech/service/impl/ResourcePicServiceImpl.java
@@ -1,8 +1,6 @@
 package com.hashtech.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.hashtech.common.AppException;
-import com.hashtech.common.ResourceCodeClass;
 import com.hashtech.config.FileParse;
 import com.hashtech.entity.ResourcePicEntity;
 import com.hashtech.mapper.ResourcePicMapper;
@@ -34,17 +32,16 @@ public class ResourcePicServiceImpl extends ServiceImpl<ResourcePicMapper, Resou
     @Value("${server.port}")
     private String port;
     private String CUSTOM  = "/resource/pic/iconDisplay?picUrl=";
+    private String HTTP_PRE  = "http://";
     @Override
     public Map<String, String> upload(MultipartFile file) throws Exception{
         InetAddress address = InetAddress.getLocalHost();
         String ip = address.getHostAddress();
-        System.out.println(ip);
-        System.out.println(port);
         String picUrl = fileParse.uploadFile(file);
         String picPath = file.getOriginalFilename();
         ResourcePicEntity resourcePicEntity = new ResourcePicEntity();
         resourcePicEntity.setPicPath(picPath);
-        picUrl = new StringBuilder(ip).append(":").append(port).append(CUSTOM).append(picUrl).toString();
+        picUrl = new StringBuilder(HTTP_PRE).append(ip).append(":").append(port).append(CUSTOM).append(picUrl).toString();
         resourcePicEntity.setPicUrl(picUrl);
         save(resourcePicEntity);
         Map<String, String> map = new HashMap<>();
