b32f67303623ca20893619114b3fe9befa73c898
xujie <xujie-bl@bestpay.com.cn>
2022-06-14 15:08:32 +0800

    调整sql

--- data-assert-backend/db/2022_05_10/resource_pic_create_2022_05_10.sql
+++ data-assert-backend/db/2022_05_10/resource_pic_create_2022_05_10.sql
@@ -5,3 +5,6 @@ CREATE TABLE `resource_pic` (
                                 `pic_url` varchar(255) NOT NULL COMMENT '资源图标路径，minio地址',
                                 PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='资源分类图标表';
+
+use daas;
+alter table `resource_table` add column `table_update_time` datetime DEFAULT NULL COMMENT '表更新时间';
