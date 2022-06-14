b5a4dee596b55aa3fe2ff3730290783f3fe9b2c6
maoshi <maoshi@newspiral.net>
2022-06-14 16:41:14 +0800

    解决：新增权限SQL

--- data-assert-backend/db/2022_06_14/sys_resource_update_2022_05_10.sql
+++ data-assert-backend/db/2022_06_14/sys_resource_update_2022_05_10.sql
@@ -21,3 +21,10 @@ INSERT INTO `sys_resource` (`sys_resource_id`, `name`, `is_leaf`, `code`, `paren
                             `updater_depart_id`, `updater_depart_name`, `need_auth`)
 VALUES ('925059302496927747', '后端解析分类图标', 1, 'L-226', '925052739900145664', 1, '/**/resource/pic/iconDisplay', 1, 3, NULL,
         NULL, '2021-12-29 12:42:31', NULL, NULL, NULL, NULL, '2021-12-29 12:42:31', NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `sys_resource` (`sys_resource_id`, `name`, `is_leaf`, `code`, `parent_id`, `status`, `url`, `type`,
+                            `layout`, `order_by`, `remark`, `create_time`, `creator_id`, `creator_name`,
+                            `creator_depart_id`, `creator_depart_name`, `update_time`, `updater_id`, `updater_name`,
+                            `updater_depart_id`, `updater_depart_name`, `need_auth`)
+VALUES ('932342837775695873', '查询应用列表', 1, 'L-227', '925068022387834880', 1, '/**/table/setting/app/groups', 1, 3, NULL,
+NULL, now(), NULL, NULL, NULL, NULL, now(), NULL, NULL, NULL, NULL, NULL);
