f6c32d13aae5ef397ad8c5682e2d71901f5db25a
xujie <xujie-bl@bestpay.com.cn>
2022-08-23 14:12:58 +0800

    默认请求方式改为post： --story=1031215 --user=徐捷 【数据服务】-API开发与管理-支持接口封装POST类型 https://www.tapd.cn/64558269/s/1648553

--- data-assert-backend/src/main/java/com/hashtech/service/impl/TableSettingServiceImpl.java
+++ data-assert-backend/src/main/java/com/hashtech/service/impl/TableSettingServiceImpl.java
@@ -157,7 +157,7 @@ public class TableSettingServiceImpl extends ServiceImpl<TableSettingMapper, Tab
         result.setInterfaceName(tableSettingEntity.getInterfaceName());
         result.setUpdateTime(resourceTableEntity.getUpdateTime());
         result.setCreateTime(resourceTableEntity.getCreateTime());
-        result.setRequestWay(tableSettingEntity.getRequestWay()==null? RquestWayEnum.POST.getCode() : tableSettingEntity.getRequestWay());
+        result.setRequestWay(tableSettingEntity.getRequestWay());
         return BusinessResult.success(result);
     }
 
